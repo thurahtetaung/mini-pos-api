18cdc0e7a2e8a6b9b4f2f8b748148d07668ec9e8
Thura Htet Aung <thurahtetaung711@gmail.com>
2023-03-24 23:12:30 +0630

    chores: boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  console.log('Hello World');
+  return response.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
+});
